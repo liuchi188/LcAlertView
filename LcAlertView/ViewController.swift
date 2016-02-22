8135a3d4e03480574df6afce2c83bf3afb64462e
liuchi <liuchi188@139.com>
2016-02-22 16:16:16 +0800

    Initial Commit

---
+++ LcAlertView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LcAlertView
+//
+//  Created by 刘驰 on 16/2/22.
+//  Copyright © 2016年 liuchi188. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
